db3cd6ef59a41eb20b29d6211a0e6ab9042aa417
NoobCoderAk <abidakbar0001@gmail.com>
2022-03-23 22:39:13 +0800

    second commit

--- Program.cs
+++ Program.cs
@@ -23,7 +23,9 @@ namespace _20200140018_Tugas2_A
                 con = new SqlConnection("data source=DESKTOP-4U0SN21\\ABIDAKBAR;DATABASE=DB_Mall;Integrated Security = TRUE");
                 con.Open();
 
-                SqlCommand cm = new SqlCommand("create table Penyewa (Kode_Penyewa char(5) not null primary key, Nama_Penyewa varchar(50), Alamat_Penyewa varchar(255), No_KTP_Penyewa char(10), No_Telp_Penyewa char(10), Jenis_Kelamin char(1))", con);
+                SqlCommand cm = new SqlCommand("create table Penyewa (Kode_Penyewa char(5) not null primary key, Nama_Penyewa varchar(50) not null unique, Alamat_Penyewa varchar(255), No_KTP_Penyewa char(10), No_Telp_Penyewa char(10), Jenis_Kelamin char(1))"+
+                                               "create table Bangunan (Kode_Bangunan char(5) not null primary key, Nama_Bangunan varchar(20) not null unique, Lokasi_Bangunan varchar(20), Harga_Bangunan_Per_Tahun int not null unique, Harga_Bangunan_Per_Bulan int not null unique)" +
+                                               "create table Transaksi (Kode_Transaksi char(5) not null primary key, Kode_Penyewa char(5) FOREIGN KEY REFERENCES Penyewa (Kode_Penyewa), Nama_Penyewa varchar(50) FOREIGN KEY REFERENCES Penyewa (Nama_Penyewa), Kode_Bangunan char(5) FOREIGN KEY REFERENCES Bangunan (Kode_Bangunan), Nama_Bangunan varchar(20) FOREIGN KEY REFERENCES Bangunan (Nama_Bangunan), Harga_Bangunan_Per_Tahun int FOREIGN KEY REFERENCES Bangunan (Harga_Bangunan_Per_Tahun), Harga_Bangunan_Per_Bulan int FOREIGN KEY REFERENCES Bangunan (Harga_Bangunan_Per_Bulan))", con);
                 cm.ExecuteNonQuery();
 
 
@@ -31,7 +33,6 @@ namespace _20200140018_Tugas2_A
                  " create table Bangunan (Kode_Bangunan char(5) not null primary key, Nama_Bangunan varchar(20), Lokasi_Bangunan varchar(20), Harga_Bangunan_Per_Tahun int, Harga_Bangunan_Per_Bulan int)" +
                     "create table Transaksi (Kode_Transaksi char(5) not null primary key, Kode_Penyewa char(5) FOREIGN KEY REFERENCES Penyewa (Kode_Penyewa), Nama_Penyewa varchar(50) FOREIGN KEY REFERENCES Penyewa (Nama_Penyewa), Kode_Bangunan char(5) FOREIGN KEY REFERENCES Bangunan (Kode_Bangunan), Nama_Bangunan varchar(20) FOREIGN KEY REFERENCES Bangunan (Nama_Bangunan), Harga_Bangunan_Per_Tahun int FOREIGN KEY REFERENCES Bangunan (Harga_Bangunan_Per_Tahun), Harga_Bangunan_Per_Bulan int FOREIGN KEY REFERENCES Bangunan (Harga_Bangunan_Per_Bulan))"
                  */
-
                 Console.WriteLine("Tabel sukses dibuat!");
                 Console.ReadKey();
             }
